b8db14b67c8aefbca5d2f5052be503919326e1f9
arifpatanduk <arifpatanduk2@gmail.com>
2024-09-20 01:05:48 +0700

    add city data

--- lib/cityData.ts
+++ lib/cityData.ts
@@ -6,5 +6,12 @@ export const cities: City[] = [
   { name: "Teminabuan", lat: "-1.5298", lon: "132.0444" },
   { name: "Maybrat", lat: "-0.6106", lon: "132.0150" },
   { name: "Tambrauw", lat: "-0.7087", lon: "132.3555" },
-  // Add more cities as needed
+  { name: "Aimas", lat: "-0.9891", lon: "131.3659" },
+  { name: "Bintuni", lat: "-2.1083", lon: "133.5220" },
+  { name: "Fak-Fak", lat: "-2.9216", lon: "132.2920" },
+  { name: "Kaimana", lat: "-3.6165", lon: "133.7107" },
+  { name: "Manokwari", lat: "-0.8913", lon: "134.0399" },
+  { name: "Pegunungan Arfak", lat: "-1.3834", lon: "133.8737" },
+  { name: "Ransiki", lat: "-1.5093", lon: "134.1761" },
+  { name: "Wondama", lat: "-2.8277", lon: "134.5357" },
 ];
